83f23d6f9368792b608d6d6e5bf93572d526d8e4
MoumitaMurmu <mmoumita202016@gmail.com>
2025-03-18 18:26:52 +0530

    favicon

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,38 @@
-const Dashboard = async () => {
-  const myData = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await myData.json();
+"use client";
+
+import { useEffect, useState } from "react";
+
+const Dashboard = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setUsers(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <div>
       <h2>User Details</h2>
+
+      <style jsx>
+        {`
+          h2 {
+            color: yellow;
+          }
+        `}
+      </style>
+
       <div>
         <ul>
           {users.map((user) => (
